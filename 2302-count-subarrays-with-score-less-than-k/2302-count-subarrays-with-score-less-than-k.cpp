e5ccd86ce397ce0c5f216a6352f016921f3e0cb3
HJ_ANAND <2304920100088@aktuacin.onmicrosoft.com>
2026-06-25 21:46:41 +0530

    Time: 4 ms (40.07%), Space: 98.9 MB (98.9%) - LeetHub

---
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        int n = nums.size();
+        int i = 0;
+        long long sum = 0, ans = 0;
+
+        for(int j = 0; j < n; j++){
+            sum += nums[j];
+
+            while(sum * (j-i+1) >= k){
+                sum -= nums[i++];
+            }
+
+            ans += (j-i+1);
+        }
+
+        return ans;
+    }
+};
