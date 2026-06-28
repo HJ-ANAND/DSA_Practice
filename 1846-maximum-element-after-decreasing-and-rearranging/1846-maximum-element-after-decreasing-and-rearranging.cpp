d9f5dee9c4ef128d7f69ee566ff18b23b00e3758
HJ_ANAND <2304920100088@aktuacin.onmicrosoft.com>
2026-06-28 23:04:35 +0530

    Time: 16 ms (7.34%), Space: 55.1 MB (69.21%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+
+        if(arr[0] != 1) arr[0] = 1;
+
+        for(int i = 1; i < n; i++){
+            int diff = abs(arr[i] - arr[i-1]);
+
+            if(diff > 1) arr[i] = arr[i-1]+1;
+        }
+
+        return arr[n-1];
+    }
+};
