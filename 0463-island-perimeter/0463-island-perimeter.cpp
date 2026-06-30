d4176a05a5ec6df643949939526c94cd362b57b6
HJ_ANAND <2304920100088@aktuacin.onmicrosoft.com>
2026-06-30 22:52:46 +0530

    Time: 5 ms (47.91%), Space: 101.9 MB (84.79%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>> &grid, int r, int c){
-        int row = grid.size();
-        int col = grid[0].size();
-
-        if(r < 0 || r >= row || c < 0 || c >= col || grid[r][c] == 0) return 1;
-
-        if(grid[r][c] == -1) return 0;
-
-        grid[r][c] = -1;
-
-        return dfs(grid, r, c+1)
-        + dfs(grid, r, c-1)
-        + dfs(grid, r+1, c)
-        + dfs(grid, r-1, c);
-    }
-
     int islandPerimeter(vector<vector<int>>& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
-        
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                if(grid[i][j]) return dfs(grid, i, j);
-            }
-        }           
+        int r = grid.size();
+        int c = grid[0].size();
 
-        return 0;
+        int para = 0;
+
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                if(grid[i][j]){
+                    para += 4;
+
+                    if(j + 1 < c && grid[i][j] && grid[i][j+1]){
+                        para -= 2;
+                    }
+                    if(i + 1 < r && grid[i][j] && grid[i+1][j]){
+                        para -= 2;
+                    }
+                }
+            }
+        }
+
+        return para;
     }
 };
