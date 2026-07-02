6c20880869920e0eebf8f6e540be973fa51ddc0e
HJ_ANAND <2304920100088@aktuacin.onmicrosoft.com>
2026-07-02 23:25:32 +0530

    Time: 4 ms (47.54%), Space: 10.1 MB (36.07%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 1) return "1";
+
+        string say = countAndSay(n-1);
+
+        string ans = "";
+
+        for(int i = 0; i < say.size(); i++){
+            char c = say[i];
+            int count = 1;
+
+            while(i < say.size()-1 && say[i] == say[i+1]) {
+                count++;
+                i++;
+            }
+
+            ans += (to_string(count) + c);
+        }
+
+        return ans;
+    }
+};
