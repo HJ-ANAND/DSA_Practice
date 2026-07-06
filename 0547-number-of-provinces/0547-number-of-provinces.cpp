6f8767456877f2a800fa2905c884aca9fdfeb077
HJ_ANAND <2304920100088@aktuacin.onmicrosoft.com>
2026-07-07 02:03:29 +0530

    Time: 0 ms (100%), Space: 19.8 MB (51.6%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> solve(vector<vector<int>> &mat){
-        int n = mat.size();
-        vector<vector<int>> ans(n, vector<int>(n ,0));
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(mat[i][j] == 1){
-                    ans[i].push_back(j);
-                    ans[j].push_back(i);
-                }
-            }
-        }
-
-        return ans;
-    }
-
-    void BFS(vector<vector<int>> adj, vector<bool> &vis, int s){
+    void BFS(vector<vector<int>> &isConnected, vector<bool> &vis, int s, int n){
         queue<int> q;
         q.push(s);
         vis[s] = true;
@@ -25,28 +9,25 @@ public:
             int curr = q.front();
             q.pop();
 
-            for(auto &e : adj[curr]){
-                if(!vis[e]){
-                    vis[e] = true;
-                    q.push(e);
+            for(int i = 0; i < n; i++){
+                if(isConnected[curr][i] && !vis[i]){
+                    vis[i] = true;
+                    q.push(i);
                 }
             }
         }
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
-        vector<vector<int>> adj = solve(isConnected);
-        
         int n = isConnected.size();
         int count = 0;
 
         vector<bool> vis(n, false);
 
         for(int i = 0; i < n; i++){
-            if(!vis[i]){
-                BFS(adj, vis, i);
-                count++;
-            }
+            if(vis[i]) continue;
+            BFS(isConnected, vis, i, n);
+            count++;
         }
 
         return count;
