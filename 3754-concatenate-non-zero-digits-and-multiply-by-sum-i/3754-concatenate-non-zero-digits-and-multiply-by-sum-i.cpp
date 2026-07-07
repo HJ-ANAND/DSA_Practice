88093ad82e706a1c454b8a70a8bd854040646482
HJ_ANAND <2304920100088@aktuacin.onmicrosoft.com>
2026-07-08 00:22:50 +0530

    Time: 0 ms (100%), Space: 9.4 MB (16.67%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     long long sumAndMultiply(int n) {
         if(!n) return n;
-        long long temp = n;
         long long sum = 0;
-
-        while(temp > 0){
-            int rem = temp % 10;
-            if(rem) sum += rem;
-            temp /= 10;
-        }
 
         string res;
 
         for(char c : to_string(n)){
             if(c != '0'){
                 res += c;
+                sum += (c - '0');
             }
         }
 
