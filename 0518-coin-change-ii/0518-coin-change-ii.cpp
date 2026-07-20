dd55d02742a90b4dde9f056749412749a747cb01
HJ_ANAND <2304920100088@aktuacin.onmicrosoft.com>
2026-07-21 00:19:58 +0530

    Time: 35 ms (41.41%), Space: 36.4 MB (62.92%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -17,7 +17,28 @@ public:
 
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        return solve(n, coins, amount, dp);
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+
+        for(int i = 0; i < n; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i <= amount; i++){
+            if(i % coins[0] == 0) dp[0][i] = 1;
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j <= amount; j++){
+                int skip = dp[i-1][j];
+                int take = 0;
+                if(coins[i] <= j){
+                    take = dp[i][j - coins[i]];
+                }
+
+                dp[i][j] = 1LL * take + skip;
+            }
+        }
+
+        return dp[n-1][amount];
     }
 };
