560edd65cc25a2d32bbd6234b95e66097d03e639
HJ_ANAND <2304920100088@aktuacin.onmicrosoft.com>
2026-07-22 13:49:24 +0530

    Time: 0 ms (100%), Space: 10.5 MB (64.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -13,8 +13,20 @@ public:
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1, -1);
+        vector<int> dp(n+1, 0);
 
-        return solve(n, nums, dp);
+        if(n == 1) return nums[0];
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i = 2; i < n; i++){
+            int take = nums[i] + dp[i-2];
+            int skip = dp[i-1];
+
+            dp[i] = max(take, skip);
+        }
+
+        return dp[n-1];
     }
 };
