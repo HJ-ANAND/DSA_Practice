1046b71377f9fe43bdd19c8a278f0275c9a3ccea
HJ_ANAND <2304920100088@aktuacin.onmicrosoft.com>
2026-08-07 01:57:17 +0530

    Time: 332 ms (30.96%), Space: 364 MB (17.39%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    vector<vector<int>> makeadj(vector<vector<int>> edges, int n){
+        vector<vector<int>> ans(n);
+
+        for(auto &e : edges){
+            int u = e[0];
+            int v = e[1];
+            ans[u].push_back(v);
+        }
+
+        return ans;
+    }
+
+    void solve(vector<vector<int>> &adj, vector<bool> &vis, vector<int> &sus, int st){
+        vis[st] = true;
+        sus.push_back(st);
+
+        for(auto &i : adj[st]){
+            if(!vis[i]) solve(adj, vis, sus, i);
+        }
+    }
+
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> adj = makeadj(invocations, n);
+
+        vector<int> sus;
+        vector<bool> vis(n, false);
+
+        solve(adj, vis, sus, k);
+
+        vector<int> ans;
+
+        bool found = false;
+
+        for(auto &e : invocations){
+            int from  = e[0];
+            int to = e[1];
+
+            if(!vis[from] && vis[to]){
+                found = true;
+                break;
+            }
+        }
+
+        if(found){
+            for(int i = 0; i < n; i++) ans.push_back(i);
+        }else{
+            for(int i = 0; i < n; i++){
+                if(!vis[i]) ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
