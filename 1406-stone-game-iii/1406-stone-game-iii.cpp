03fb36ac07597c0218b4ba2202ada9dcf5b494c5
HJ_ANAND <2304920100088@aktuacin.onmicrosoft.com>
2026-08-07 16:14:12 +0530

    Time: 44 ms (22.5%), Space: 152.7 MB (6.94%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int n;
+    int solve(vector<int> &stone, int i, int res, vector<int> &dp){
+        if(i >= n) return 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        res = max(res, stone[i] - solve(stone, i + 1, res, dp));
+        if(i+1 < n)
+        res = max(res, stone[i] + stone[i+1] - solve(stone, i + 2, res, dp));
+        if(i+2 < n)
+        res = max(res, stone[i] + stone[i+1] + stone[i+2] - solve(stone, i + 3, res, dp));
+
+        return dp[i] = res;
+    }
+
+    string stoneGameIII(vector<int>& stone) {
+        n = stone.size();
+        vector<int> dp(n+1, -1);
+
+        int ans = solve(stone, 0, INT_MIN, dp);
+
+        if(ans > 0) return "Alice";
+        else if(ans == 0) return "Tie";
+
+        return "Bob"; 
+    }
+};
